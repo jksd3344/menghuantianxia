4282b16e35e8e0d4226f5ed3cf5b9672cffab903
liuxin <jiayuguang@qq.com>
2020-09-23 19:04:25 +0800

    setting genggai

--- test_django/test_show/settings.py
+++ test_django/test_show/settings.py
@@ -56,7 +56,7 @@ WSGI_APPLICATION = 'test_show.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-# DATABASES = {
+# DATABASES ={
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
